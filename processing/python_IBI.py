e71ad6acef8b5996020b61ad1056c4927af416f4
jfrey <nomhad@gmail.com>
2015-07-08 10:14:59 +0200

    HRV: implementing safeguards

--- processing/python_IBI.py
+++ processing/python_IBI.py
@@ -19,7 +19,8 @@ class MyOVBox(OVBox):
       self.signalHeader = None
       self.IBIvalue = 1.
       self.BPMvalue = 1./self.IBIvalue*60
-      self.lastStimDate = 0
+      self.lastStimDate = -1
+      self.newStimDate = 0
 
    # this time we also re-define the initialize method to directly prepare the header and the first data chunk
    def initialize(self):
@@ -29,6 +30,12 @@ class MyOVBox(OVBox):
       # settings are retrieved in the dictionary
       self.samplingFrequency = int(self.setting['Sampling frequency'])
       self.epochSampleCount = int(self.setting['Generated epoch sample count'])
+      
+      # safeguards (-1 to disable)
+      self.minBPM = int(self.setting['Min BPM'])
+      self.maxBPM = int(self.setting['Max BPM'])
+      # in percentage, max BPM variation between two seconds
+      self.maxVariation = int(self.setting['Max variation'])
          
       #creation of the signal header
       self.dimensionLabels.append('IBI') 
@@ -64,13 +71,45 @@ class MyOVBox(OVBox):
       self.output[0].append( OVSignalBuffer(start, end, bufferElements) )
 
 
-   # called by process for each stim reiceived
-   # send stimulations to external world
+   # called by process for each stim reiceived; update timestamp of last stim
    def trigger(self, stim):
-     #print "Got stim: ", stim.identifier, " date: ", stim.date, " duration: ", stim.duration
-     self.IBIvalue = stim.date - self.lastStimDate
-     self.lastStimDate = stim.date
-     self.BPMvalue = 1./self.IBIvalue*60
+     print "Got stim: ", stim.identifier, " date: ", stim.date, " duration: ", stim.duration
+     self.newStimDate = stim.date
+     self.updateValues()
+   
+   # called by process each loop or by trigger when got new stimulation;  update IBI/BPM
+   def updateValues(self):
+     BPMvalue = self.BPMvalue
+     # when called by trigger()
+     if self.newStimDate != self.lastStimDate:
+       BPMvalue = 1./(self.newStimDate - self.lastStimDate)*60
+       # safeguard, if too short
+       self.lastStimDate = self.newStimDate
+       
+     # safeguard, if too long
+     if self.maxVariation >=0:
+       print "laststim", self.lastStimDate, ", new:", self.newStimDate
+       nextStim = self.lastStimDate + 1./(BPMvalue-BPMvalue*self.maxVariation/100)*60
+       print "nextstim: ", nextStim
+       if self.getCurrentTime() >= nextStim:
+         self.newStimDate = self.getCurrentTime()
+         BPMvalue = 1./(self.newStimDate - self.lastStimDate)*60
+         self.lastStimDate = self.newStimDate
+         print "safe guard long! ", self.getCurrentTime(), " BPM: ", BPMvalue
+     
+     # safeguards for min/max
+     if self.minBPM >= 0 and BPMvalue < self.minBPM:
+       BPMvalue = self.minBPM
+     if self.maxBPM >= 0 and BPMvalue > self.maxBPM:
+       BPMvalue = self.maxBPM
+       
+     # update internal state
+     self.BPMvalue = BPMvalue
+     if self.BPMvalue != 0:
+       self.IBIvalue = 1./BPMvalue*60
+     else:
+       self.IBIvalue = 0
+     
 
    # the process is straightforward
    def process(self):
@@ -97,6 +136,8 @@ class MyOVBox(OVBox):
       start = self.timeBuffer[0]
       end = self.timeBuffer[-1]
       if self.getCurrentTime() >= end:
+         # in case we need to automatically change BPM 'cause of min/max
+         self.updateValues()
          # send buffer
          self.sendSignalBufferToOpenvibe()
          self.updateStartTime()
